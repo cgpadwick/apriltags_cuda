b1beb1b20947e58e13e556b584346bf0a0e8d36a
Max Spier <68516760+Mixmix00@users.noreply.github.com>
2024-09-04 19:23:05 -0700

    try new

--- src/ntexample.cpp
+++ src/ntexample.cpp
@@ -1,40 +1,10 @@
 #include "networktables/NetworkTable.h"
-#include "networktables/DoubleTopic.h"
 #include "networktables/NetworkTableInstance.h"
 #include "networktables/DoubleArrayTopic.h"
 
 #include <string>
 #include <iostream>
-
-/*
-Right now, the table is /SmartDashboard so we can visualize the values sent from the Orin.
-Later, this should be changed to a new table, such as /Orin or /Vision, to avoid populating the SmartDashboard with unnecessary values
-*/
-
-// class DoubleValueSender{
-//   private:
-//     nt::NetworkTableInstance inst;
-//     nt::DoublePublisher publisher;
-//   public:
-//     DoubleValueSender(std::string key){
-//       inst = nt::NetworkTableInstance::GetDefault();
-//       inst.SetServer("10.7.66.2");
-//       inst.StartClient4("10.7.66.2");
-//       auto table = inst.GetTable("/SmartDashboard");
-//       nt::DoubleTopic topic = table->GetDoubleTopic(key);
-      
-//       publisher = topic.Publish();
-//     }
-
-//     void sendValue(double value){
-//       publisher.Set(value);
-//     }
-
-//     void setDefaultValue(double value){
-//       publisher.SetDefault(value);
-//     }
-  
-// };
+#include <vector>
 
 class DoubleArraySender{
   private:
@@ -57,20 +27,6 @@ class DoubleArraySender{
     }
 };
 
-/*
-*
-* Example on how to use each class
-*
-*/
-
-// int main(){
-//   DoubleValueSender sender("NVIDIA ORIN TEST");
-//   while(2>1){
-//     sender.sendValue(1.0);
-//     std::cout << "Sent value" << std::endl;
-//   }
-//   return 0;
-// }
 
 int main(){
   DoubleArraySender sender("NVIDIA ORIN ARRAY TEST");
