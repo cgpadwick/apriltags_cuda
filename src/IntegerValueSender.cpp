8ecb5d2d3c2ee100cc1f826580e5d06eb0cc0865
Max Spier <68516760+Mixmix00@users.noreply.github.com>
2024-09-18 19:34:32 -0700

    i'm so stupid

--- src/IntegerValueSender.cpp
+++ src/IntegerValueSender.cpp
@@ -15,11 +15,11 @@ IntegerValueSender::IntegerValueSender(std::string key){
   publisher_ = topic.Publish();
 }
 
-void IntegerValueSender::sendValue(std:vector<int> value){
+void IntegerSender::sendValue(int value){
   publisher_.Set(value);
 }
 
-void IntegerValueSender::setDefaultValue(std::vector<int> value){
+void IntegerValueSender::setDefaultValue(int value){
   publisher_.SetDefault(value);
 }
 
